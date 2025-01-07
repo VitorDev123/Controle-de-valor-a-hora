b730f87afb687117983dce4083248924b636f3e4
VitorDev123 <VitorDevmagalhaes@gmail.com>
2025-01-07 09:43:49 -0300

    Add files via upload

---
+++ Controle de horas/Program.cs
@@ -0,0 +1,13 @@
+﻿
+double? vh, ht;
+
+Console.WriteLine("Escreva o valor a hora");
+vh = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Horas em que trabalhou");
+ht = double.Parse(Console.ReadLine());
+
+Console.WriteLine( ht * vh);
+ 
+
+
